285797cea2f8f6e6e30f658f166af1125f5f08ad
Simone Pasquini <simone.pasquini@onebip.com>
2022-07-08 08:28:02 +0200

    Episode 2.12
    - __toString

--- src/app/Invoice/Invoice.php
+++ src/app/Invoice/Invoice.php
@@ -6,20 +6,8 @@ namespace App\Invoice;
 
 class Invoice
 {
-    protected function process(float $amount, string $description)
+    public function __toString()
     {
-        var_dump($amount, $description);
-    }
-
-    public function  __call($name, $arguments)
-    {
-        if (method_exists($this, $name)) {
-            call_user_func_array([$this, $name], $arguments);
-        }
-    }
-
-    public static function __callStatic($name, $arguments)
-    {
-        var_dump('static', $name, $arguments);
+        return 'Invoice number 2';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -6,6 +6,4 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new Invoice();
 
-$invoice->process(7, 'Transaction 1');
-
-// $invoice::processTwo(1, 2, 3);
+echo $invoice . PHP_EOL;
