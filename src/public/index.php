083b4d507ed668c667240bebef4f86ff5139cb35
Simone Pasquini <simone.pasquini@onebip.com>
2022-07-13 07:37:32 +0200

    Episode 2.21
    - DateInterval

--- src/public/index.php
+++ src/public/index.php
@@ -3,10 +3,13 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $date1 = '12/05/2022 3:30 AM';
-$date2 = '11/05/2022 3:35 AM';
 
 $dateTime1 = new DateTime($date1);
-$dateTime2 = new DateTime($date2);
 
-var_dump($dateTime1->diff($dateTime2));
-echo $dateTime1->diff($dateTime2)->format('%R%a') . PHP_EOL;
+$dateInterval = new DateInterval('P1M2D');
+
+echo $dateTime1->add($dateInterval)->format(DateTime::ISO8601) . PHP_EOL;
+
+$dateInterval->invert = 1;
+
+echo $dateTime1->add($dateInterval)->format(DateTime::ISO8601) . PHP_EOL;
