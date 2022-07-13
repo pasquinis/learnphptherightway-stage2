3cbe04ed5bc4d967550e804ee3a0fd60e818c44c
Simone Pasquini <simone.pasquini@onebip.com>
2022-07-13 07:12:57 +0200

    Episode 2.21
    - diff -> format

--- src/public/index.php
+++ src/public/index.php
@@ -3,12 +3,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $date1 = '12/05/2022 3:30 AM';
-$date2 = '12/05/2022 3:35 AM';
+$date2 = '11/05/2022 3:35 AM';
 
 $dateTime1 = new DateTime($date1);
 $dateTime2 = new DateTime($date2);
 
-var_dump($dateTime1 < $dateTime2);
-var_dump($dateTime1 > $dateTime2);
-var_dump($dateTime1 == $dateTime2);
-var_dump($dateTime1 <=> $dateTime2);
+var_dump($dateTime1->diff($dateTime2));
+echo $dateTime1->diff($dateTime2)->format('%R%a') . PHP_EOL;
