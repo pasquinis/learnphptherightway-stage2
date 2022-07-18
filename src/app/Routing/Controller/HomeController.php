feceebc94a9f5cffa4691dc9eb6ba87e107e41fb
Simone Pasquini <simone.pasquini@onebip.com>
2022-07-18 11:51:14 +0200

    Episode 2.31
    - PDO beginTransaction

--- src/app/Routing/Controller/HomeController.php
+++ src/app/Routing/Controller/HomeController.php
@@ -7,8 +7,10 @@ namespace App\Routing\Controller;
 use App\Routing\View;
 use DateTime;
 use DateTimeZone;
+use Exception;
 use PDO;
 use PDOException;
+use Throwable;
 
 class HomeController
 {
@@ -16,32 +18,62 @@ class HomeController
     {
         try {
             $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root');
-
-            $query = 'INSERT INTO users(email, full_name, is_active, created_at)
-                VALUES (:email, :name, :active, :date)';
-            $stmt = $db->prepare($query);
-
-            $email = 'j3.doe@email.com';
-            $fullName = 'Jim Three';
-            $isActive = true;
-            $createdAt = (new DateTime('now', new DateTimeZone('UTC')))->format('Y-m-d H:i:s');
-
-            $stmt->bindValue(':email', $email);
-            $stmt->bindValue(':name', $fullName);
-            $stmt->bindValue(':active', $isActive, PDO::PARAM_BOOL);
-            $stmt->bindValue(':date', $createdAt);
-
-            $stmt->execute();
-
-            $id = (int) $db->lastInsertId();
-
-            $user = $db->query('SELECT * FROM users WHERE id = ' . $id)->fetch();
-            echo '<pre>';
-            var_dump($user);
-            echo '</pre>';
         }
         catch (PDOException $e) {
             throw new PDOException($e->getMessage(), $e->getCode());
+        }
+
+        try {
+            $db->beginTransaction();
+            $email = mt_rand(1, 100) . 'j.doe@email.com';
+            $fullName = 'Jim Three' . mt_rand(1, 100);
+            $isActive = true;
+            $createdAt = (new DateTime('now', new DateTimeZone('UTC')))->format('Y-m-d H:i:s');
+
+            $query = 'INSERT INTO users(email, full_name, is_active, created_at)
+                VALUES (:email, :name, :active, :date)';
+            $usertStmnt = $db->prepare($query);
+            $usertStmnt->bindValue(':email', $email);
+            $usertStmnt->bindValue(':name', $fullName);
+            $usertStmnt->bindValue(':active', $isActive, PDO::PARAM_BOOL);
+            $usertStmnt->bindValue(':date', $createdAt);
+
+            $usertStmnt->execute();
+
+            $userId = (int)$db->lastInsertId();
+
+            $amount = mt_rand(1, 300);
+            $invoiceQuery = 'INSERT INTO invoices(amount, user_id)
+                VALUES (:amount, :userId)';
+            $invoiceStmnt = $db->prepare($invoiceQuery);
+
+            $invoiceStmnt->bindValue(':amount', $amount);
+            $invoiceStmnt->bindValue(':userId', $userId);
+
+            $invoiceStmnt->execute();
+            $db->commit();
+        }
+        catch(Throwable $e) {
+            if ($db->inTransaction()) {
+                $db->rollBack();
+            }
+
+            throw $e;
+        }
+
+        $invoiceUserQuery = 'SELECT i.amount, u.full_name, u.id FROM invoices AS i
+                INNER JOIN users AS u ON u.id = i.user_id
+                WHERE u.email = :email';
+
+        $selectStmnt = $db->prepare($invoiceUserQuery);
+        $selectStmnt->bindValue(':email', $email);
+
+        $selectStmnt->execute();
+
+        foreach ($selectStmnt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+            echo '<pre>';
+            var_dump($row);
+            echo '</pre>';
         }
 
         return View::make('index', [
